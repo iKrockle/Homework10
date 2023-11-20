18464cb37736439b58f2d5f39d3de4a67c24d2e5
Артем <forsakl123@gmail.com>
2023-11-20 11:46:18 +0500

    tasks done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName+" "+middleName+" "+lastName;
+
+        //task 1
+        System.out.println("Ф. И. О. сотрудника — "+fullName);
+
+        //task2
+        System.out.println("Ф. И. О. сотрудника — "+fullName.toUpperCase());
+
+        fullName = "Иванов Семён Семёнович";
+
+        //task3
+        System.out.println("Ф. И. О. сотрудника — "+fullName.replace("ё","е"));
+
     }
 }
